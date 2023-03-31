c7d26c285ca7bddadf38527a1d14792dfffcdfca
Shervin Fernando <shervin@Shervins-MacBook-Pro.local>
2023-03-31 18:52:04 +1100

    2nd deploy

---
+++ precache-manifest.e89dc45e44d0c9214078adec0bd30de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c384f7baa37fd5b7dba",
+    "url": "/chatgpt-test/static/css/main.676274e2.chunk.css"
+  },
+  {
+    "revision": "7c384f7baa37fd5b7dba",
+    "url": "/chatgpt-test/static/js/main.7c384f7b.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/chatgpt-test/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "11a3eb75ba923fb4e350",
+    "url": "/chatgpt-test/static/js/2.11a3eb75.chunk.js"
+  },
+  {
+    "revision": "76ce6550d50fae089eb4",
+    "url": "/chatgpt-test/static/js/runtime~main.76ce6550.js"
+  },
+  {
+    "revision": "f95fa82dd2892eeda1beae274551e4b4",
+    "url": "/chatgpt-test/index.html"
+  }
+];
